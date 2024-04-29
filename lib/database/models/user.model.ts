f7d75d345f5590e23a6808030e78e8a01872bfc5
sark-07 <sarkarpritam0007@gmail.com>
2024-04-30 01:00:01 +0530

    Server actions for the user has been added

---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,31 @@
+import { Document, Schema, model, models } from "mongoose";
+
+
+
+export interface IUser extends Document{
+    clerkId: string;
+    email: string;
+    userName: string;
+    photo: string;
+    firstName: string;
+    lastName: string;
+    planId?: string; 
+    creditBalance?: number; 
+}
+
+
+const UserSchema = new Schema({
+    clerkId: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
+    userName: {type: String, required: true, unique: true},
+    photo: {type: String, required: true},
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    planId: {type: Number, default: 1},
+    creditBalance: {type: Number, default: 15},
+})
+
+
+const User = models?.User || model('User', UserSchema);
+
+export default User;
